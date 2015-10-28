2547c4c1ab784059342184c97e5279a7f925cbc7
Masiur Rahman Siddiki <masiur.sust@csesociety.org>
2015-10-28 10:45:00 +0600

    fff

--- Evaluate.php
+++ Evaluate.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <head>
-	<title>Complete Your Registration</title>
+	<title>Evaluation Page || Dept. of CSE, SUST</title>
 </head>
 <body>
 	<form method="post" action="action.php">
-		<label for="email">Give Your Email Address once you Provided</label><br><br>
-		<input type="email" name="email" id="email" required><br><br>
-		<label for="ukey" >Provide Your Unique Key & Complete Registation Process</label><br><br>
+		<label for="email">Enter the passkey provided to You</label><br><br>
 
 		<input type="text" id="ukey" name="ukey" required>
 		<span class="error"></span>
