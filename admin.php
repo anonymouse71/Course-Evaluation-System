d9a8002a3b783c363c82292c94bbcad1e1d93e59
Abu Hanife <simplenayem@gmail.com>
2015-11-08 02:24:07 +0600

    kigh

--- admin.php
+++ admin.php
@@ -21,26 +21,31 @@ if(!isset($_SESSION['myusername']) ){
   <div class="well well-lg">
   <h2>SUST CSE Course Evolution</h2>
   <h4>Admin only</h4>
+
 </div>
 
 <center><?php echo "<h3> Welcome ". $_SESSION['myusername']." </h3>"; ?></center>
+<a href="logout.php"><center>Logout</center></a>
 
 <form action="generate.php" method="post">
 <br>
+
 <br><h4>Genarate some token:</h4><br>
 
 <?php echo " &nbsp &nbsp &nbsp &nbsp Form Link: &nbsp &nbsp &nbsp&nbsp&nbsp"; ?> <input type="text" name="form_link"><br><br>
+
 <?php  echo"&nbsp &nbsp &nbsp &nbsp" ?> Token amount: <input type="number" name="pass_amount"><br>
 <br>
  <?php echo"&nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp" ?>
 <input type="submit" value="Generate">
 </form>
+
 <br>
 <br><br>
 
 <a href="another_user.php"><center>Add another user</center></a>
 <br><br>
-<a href="logout.php"><center>Logout</center></a>
+
 </div>
 </body>
 </html>
