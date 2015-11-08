383d3a6870df6fd9de01e83f8a4c2a3a5cf62ec9
Abu Hanife <simplenayem@gmail.com>
2015-11-08 09:44:27 +0600

    df

--- go.php
+++ go.php
@@ -6,13 +6,24 @@
 	<script src="bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-<center>
-   <form method="post" action="finalcheck.php" method="POST">
-   <br><br>
-   Token: <input type="text" name="token" required>
-   <br><br>
-   <input type="submit" name="submit" value="CONTINUE"> 
-</form>
-</center>
+<div class="container">
+  <h2 style="text-align:center">Course Evaluation System</h2><br><br>
+
+  <div class="col-md-6 col-md-offset-3">
+  <form class="form-horizontal" action="finalcheck.php" method="POST">
+      <div class="form-group">
+        <label class="control-label col-sm-2" for="token">Token:</label>
+          <div class="col-sm-8">
+            <input type="text" name="token" class="form-control" id="token" placeholder="Enter provided token">
+          </div>
+      </div>
+      <div class="form-group"> 
+        <div class="col-sm-offset-2 col-xs-4">
+          <button type="submit" class="btn btn-primary">Continue</button>
+        </div>
+      </div>
+  </form>
+  </div>
+</div>
 </body>
 </html>
