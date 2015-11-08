612cc7b0b6d5192959198dac67789b9c3abe515e
Abu Hanife <simplenayem@gmail.com>
2015-11-08 09:28:04 +0600

    fsadj

--- another_user.php
+++ another_user.php
@@ -16,14 +16,25 @@ if(!isset($_SESSION['myusername']) ){
  
  <!--It is only for form input of new user-->
 
+<div class="container">
+<div class="well well-lg">
 <center>
+  <h2>SUST CSE Course Evolution</h2>
+  <h4>Admin only</h4>
+</center>
+</div>
+
+
+<center>
+<h3>Add another user</h3>
 <form action="add_user.php" method="post">
 <br><br><br>
 Username: <input type="text" name="admin_name"><br><br>
 Password: <input type="text" name="admin_pass"><br><br>
-<input type="submit" value="Create">
-</center>
+<input type="submit" value="Add it">
 <!--Developed by: Md Abu Hanife Nayem & Masiur rahman siddiki-->
 </form>
+</center>
+</div>
 </body>
 </html>
