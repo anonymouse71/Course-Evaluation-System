48837ecea209d2fc4c3ade3e19bf9b3d18002385
Abu Hanife <simplenayem@gmail.com>
2015-11-08 09:39:26 +0600

    fg

--- login.php
+++ login.php
@@ -6,20 +6,43 @@
 	<script src="bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <div class="container">
-  <div class="jumbotron">
-    <h1>SUST CSE Course Evolution</h1> 
-    <h2>Admin Login</h2>
-  </div>
-	<form action="checklogin.php" method="post">
-		  <br><br>
-		  <input type="text" name="username" placeholder="Username">
-		  <br>
-		  <br>
-		  <input type="password" name="password" placeholder="Password">
-		  <br><br>
-		  <input type="submit" value="Submit">
-	</form>
+    
+<div class="container">
+
+  <div class="well well-lg">
+	  <h2 style="text-align:center">Course Evaluation System</h2>
+	  <h4 style="text-align:center">Admin login</h4>
+
+  </div> 
+<h4 style="text-align:center">Please, provide username & password </h4>
+<br><br>
+<div class="col-md-6 col-md-offset-3">
+<form class="form-horizontal" action="checklogin.php" method="post">
+	<div class="form-group">
+		<label class="control-label col-sm-2" for="username">Username:</label>
+		<div class="col-sm-8">
+			<input type="text" name="username" class="form-control" placeholder="Enter Username" required>
+		</div>
 	</div>
+
+	<div class="form-group">
+		<label class="control-label col-sm-2" for="password">Password: </label>
+		<div class="col-sm-8">
+			<input type="password" name="password" class="form-control" placeholder="Enter Password" required>
+		</div>
+	</div>
+
+	<div class="form-group"> 
+        <div class="col-sm-offset-2 col-xs-4">
+          <button type="submit" class="btn btn-primary">Login</button>
+        </div>
+     </div>
+
+</form>
+
+</div>
+</div>
+
+
 </body>
 </html>
